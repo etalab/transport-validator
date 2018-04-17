c1a75de557e89eb79717a0acb29e6fc73176fc0c
Tristram Gräbener <tristramg@gmail.com>
2018-04-17 13:15:31 +0200

    Validate (CLI) from url

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,14 @@ struct Opt {
 
 fn main() {
     let opt = Opt::from_args();
-    let gtfs = if opt.input.to_lowercase().ends_with(".zip"){
+    let gtfs = if opt.input.starts_with("http") {
+        gtfs_structures::Gtfs::from_url(&opt.input)
+    } else if opt.input.to_lowercase().ends_with(".zip"){
         gtfs_structures::Gtfs::from_zip(&opt.input)
-    }
-    else{
+    } else {
         gtfs_structures::Gtfs::new(&opt.input)
     };
+
     let validation = gtfs
         .map(|gtfs| validators::validate(&gtfs))
         .and_then(|validation| Ok(serde_json::to_string(&validation)?));
