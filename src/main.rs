f0290d6f6ebac2bb856c314c3995632ad7e8b502
Tristram Gräbener <tristramg@gmail.com>
2018-04-17 17:24:55 +0200

    Allow validation through an API with JSON response

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
+mod daemon;
 mod validators;
 
+extern crate failure;
+extern crate gotham;
+#[macro_use]
+extern crate gotham_derive;
 extern crate gtfs_structures;
+extern crate hyper;
 extern crate structopt;
 #[macro_use]
 extern crate structopt_derive;
 use structopt::StructOpt;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "gtfs-validator", about = "Validates the gtfs file.")]
 struct Opt {
     #[structopt(short = "i", long = "input",
                 help = "Path to the gtfs file. Can be a directory or a zip file")]
-    input: String,
+    input: Option<String>,
 }
 
 fn main() {
     let opt = Opt::from_args();
-    let gtfs = if opt.input.starts_with("http") {
-        gtfs_structures::Gtfs::from_url(&opt.input)
-    } else if opt.input.to_lowercase().ends_with(".zip") {
-        gtfs_structures::Gtfs::from_zip(&opt.input)
-    } else {
-        gtfs_structures::Gtfs::new(&opt.input)
-    };
 
-    let validation = gtfs.map(|gtfs| validators::validate(&gtfs))
-        .and_then(|validation| Ok(serde_json::to_string(&validation)?));
-    match validation {
-        Ok(json) => println!("Validation: {}", json),
-        Err(err) => println!("Error: {}", err),
+    if let Some(input) = opt.input {
+        match validators::validate(&input) {
+            Ok(json) => println!("Validation: {}", json),
+            Err(err) => println!("Error: {}", err),
+        }
+    } else {
+        println!("Starting the validator as a dæmon");
+        daemon::run_server()
     }
 }
--- src/validators/mod.rs
+++ src/validators/mod.rs
@@ -1,8 +1,24 @@
 pub mod issues;
 mod unused_stop;
 
+extern crate serde_json;
+use failure::Error;
+
 extern crate gtfs_structures;
 
-pub fn validate(gtfs: &gtfs_structures::Gtfs) -> Vec<issues::Issue> {
+pub fn validate_gtfs(gtfs: &gtfs_structures::Gtfs) -> Vec<issues::Issue> {
     unused_stop::validate(gtfs)
+}
+
+pub fn validate(input: &str) -> Result<String, Error> {
+    let gtfs = if input.starts_with("http") {
+        gtfs_structures::Gtfs::from_url(input)
+    } else if input.to_lowercase().ends_with(".zip") {
+        gtfs_structures::Gtfs::from_zip(input)
+    } else {
+        gtfs_structures::Gtfs::new(input)
+    };
+
+    gtfs.map(|gtfs| self::validate_gtfs(&gtfs))
+        .and_then(|validation| Ok(serde_json::to_string(&validation)?))
 }
