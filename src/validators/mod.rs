216ebefbc7da26d29ed509e4eafd17659875fc6e
baptisteba <brunarasa@gmail.com>
2019-01-29 14:14:17 +0100

    Minor issue creation changes
    Some issues were not created with the new issue constructors.

--- src/validators/mod.rs
+++ src/validators/mod.rs
@@ -52,14 +52,12 @@ pub fn create_issues(input: &str) -> Response {
         Ok(gtfs) => self::validate_and_metada(&gtfs),
         Err(e) => Response {
             metadata: None,
-            validations: vec![issues::Issue {
-                severity: issues::Severity::Fatal,
-                issue_type: issues::IssueType::InvalidArchive,
-                object_id: "".to_string(),
-                object_name: None,
-                related_objects: vec![],
-                details: Some(format!("{}", e)),
-            }],
+            validations: vec![issues::Issue::new(
+                issues::Severity::Fatal,
+                issues::IssueType::InvalidArchive,
+                "",
+            )
+            .details(format!("{}", e).as_ref())],
         },
     }
 }
