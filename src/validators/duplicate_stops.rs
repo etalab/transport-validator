c4bf67bdd6e29104a17c2e81c648b873c3af2640
baptisteba <brunarasa@gmail.com>
2019-02-13 11:16:58 +0100

    Removed the check on StopEntrance

--- src/validators/duplicate_stops.rs
+++ src/validators/duplicate_stops.rs
@@ -6,7 +6,12 @@ use itertools::Itertools;
 pub fn validate(gtfs: &gtfs_structures::Gtfs) -> Vec<Issue> {
     let mut issues = vec![];
 
-    for (stop_a, stop_b) in gtfs.stops.values().tuple_combinations() {
+    for (stop_a, stop_b) in gtfs
+        .stops
+        .values()
+        .filter(|stop| !(stop.location_type == gtfs_structures::LocationType::StationEntrance))
+        .tuple_combinations()
+    {
         if duplicate_stops(stop_a, stop_b) {
             issues.push(make_duplicate_stops_issue(&**stop_a).add_related_object(&**stop_b));
         }
@@ -23,12 +28,11 @@ fn duplicate_stops(stop_a: &gtfs_structures::Stop, stop_b: &gtfs_structures::Sto
 fn too_close_stops(stop_a: &gtfs_structures::Stop, stop_b: &gtfs_structures::Stop) -> bool {
     let a = Point::new(stop_a.longitude, stop_a.latitude);
     let b = Point::new(stop_b.longitude, stop_b.latitude);
-    let min_distance = match stop_a.location_type {
-        gtfs_structures::LocationType::StopPoint => 2.,
-        gtfs_structures::LocationType::StopArea => 100.,
-        gtfs_structures::LocationType::StationEntrance => 1.,
-    };
-    a.haversine_distance(&b) < min_distance
+    match stop_a.location_type {
+        gtfs_structures::LocationType::StopPoint => a.haversine_distance(&b) < 2.,
+        gtfs_structures::LocationType::StopArea => a.haversine_distance(&b) < 100.,
+        _ => false,
+    }
 }
 
 fn make_duplicate_stops_issue<
@@ -69,23 +73,6 @@ fn test_stop_areas() {
     assert_eq!(1, duplicate_stops_issues.len());
     assert_eq!(
         "Stop Area",
-        duplicate_stops_issues[0].object_name.as_ref().unwrap()
-    );
-}
-
-#[test]
-fn test_station_entrance() {
-    let gtfs = gtfs_structures::Gtfs::new("test_data/duplicate_stops").unwrap();
-    let issues = validate(&gtfs);
-    let duplicate_stops_issues: Vec<_> = issues
-        .iter()
-        .filter(|issue| issue.issue_type == IssueType::DuplicateStops)
-        .filter(|issue| issue.object_name == Some("Station Entrance".to_string()))
-        .collect();
-
-    assert_eq!(1, duplicate_stops_issues.len());
-    assert_eq!(
-        "Station Entrance",
         duplicate_stops_issues[0].object_name.as_ref().unwrap()
     );
 }
