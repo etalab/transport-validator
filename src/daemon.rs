8c15466de0d4ac8f34adcb9730b93a31528ff0b6
Tristram Gräbener <tristramg@gmail.com>
2019-03-14 18:03:51 +0100

    Rollack gotham

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,8 +1,5 @@
 use crate::validators::validate;
-use gotham::helpers::http::response::create_response;
-use gotham::middleware::logger::SimpleLogger;
-use gotham::pipeline::new_pipeline;
-use gotham::pipeline::single::single_pipeline;
+use gotham::http::response::create_response;
 use gotham::router::{builder::*, Router};
 use gotham::state::{FromState, State};
 use hyper::{Body, Response, StatusCode};
@@ -20,36 +17,37 @@ fn validation_handler(mut state: State) -> (State, Response<Body>) {
     let res = match validate(&query_param.url, query_param.max_size.unwrap_or(1000)) {
         Ok(json) => create_response(
             &state,
-            StatusCode::OK,
-            mime::APPLICATION_JSON,
-            json.into_bytes(),
+            StatusCode::Ok,
+            Some((json.into_bytes(), mime::APPLICATION_JSON)),
         ),
         Err(err) => create_response(
             &state,
-            StatusCode::INTERNAL_SERVER_ERROR,
-            mime::APPLICATION_JSON,
-            format!("{{\"error\": \"{}\"}}", err).into_bytes(),
+            StatusCode::InternalServerError,
+            Some((
+                format!("{{\"error\": \"{}\"}}", err).into_bytes(),
+                mime::APPLICATION_JSON,
+            )),
         ),
     };
     log::info!("Finnished validation: {}", &query_param.url);
     (state, res)
 }
 
-fn index(state: State) -> (State, &'static str) {
-    (
-        state,
-        "GTFS Validation tool (https://github.com/etalab/transport-validator-rust)\n
-Use it with /validation?url=https.//.../gtfs.zip",
-    )
+static BODY: &'static [u8] =
+    b"GTFS Validation tool (https://github.com/etalab/transport-validator-rust)\n
+Use it with /validation?url=https.//.../gtfs.zip";
+fn index(state: State) -> (State, Response) {
+    let res = create_response(
+        &state,
+        StatusCode::Ok,
+        Some((BODY.to_vec(), mime::TEXT_PLAIN)),
+    );
+
+    (state, res)
 }
 
 fn router() -> Router {
-    let (chain, pipelines) = single_pipeline(
-        new_pipeline()
-            .add(SimpleLogger::new(log::Level::Info))
-            .build(),
-    );
-    build_router(chain, pipelines, |route| {
+    build_simple_router(|route| {
         route
             .get("/validate")
             .with_query_string_extractor::<QueryStringExtractor>()
