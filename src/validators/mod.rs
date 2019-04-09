eb69f35b71564ce4182ee2911b3afd091cb776ed
antoine-de <antoine.desbordes@gmail.com>
2019-04-09 11:47:03 +0200

    add a global check testing the UnloadableModel error
    this checks that when we have a Fatal `UnloadableModel` error, we still
    apply some rule.
    
    This also checks that we do not flood with invalid ref on stops (since
    we were unable to load any stops)

--- src/validators/mod.rs
+++ src/validators/mod.rs
@@ -14,8 +14,12 @@ mod raw_gtfs;
 mod route_type;
 mod shapes;
 mod unused_stop;
-
+use itertools::Itertools;
 use std::collections::BTreeMap;
+
+fn pretty_print_error(error: failure::Error) -> String {
+    error.iter_chain().map(|c| format!("{}", c)).join(": ")
+}
 
 #[derive(Serialize, Debug)]
 /// Holds the issues and metadata about the GTFS.
@@ -58,7 +62,7 @@ pub fn validate_and_metadata(rgtfs: gtfs_structures::RawGtfs, max_issues: usize)
                     issues::IssueType::UnloadableModel,
                     "A fatal error has occured while loading the model, many rules have not been checked",
                 )
-                .details(format!("{}", e).as_ref()),
+                .details(&pretty_print_error(e)),
             );
         }
     }
@@ -123,4 +127,47 @@ pub fn create_issues(input: &str, max_issues: usize) -> Response {
 /// Returns a JSON with all the issues on the GTFS. Either takes an URL, a directory path or a .zip file as parameter.
 pub fn validate(input: &str, max_issues: usize) -> Result<String, failure::Error> {
     Ok(serde_json::to_string(&create_issues(input, max_issues))?)
+}
+
+// Test reading a GTFS with a broken stops.txt file.
+// we should have the RawGTFS rules applied, and a `Fatal` error on the stops.txt file
+#[test]
+fn test_invalid_stop_points() {
+    let issues = create_issues("test_data/invalid_stop_file", 1000);
+
+    let unloadable_model_errors = &issues.validations[&issues::IssueType::UnloadableModel];
+
+    assert_eq!(unloadable_model_errors.len(), 1);
+    let unloadable_model_error = &unloadable_model_errors[0];
+
+    assert_eq!(unloadable_model_error, &issues::Issue {
+            severity: issues::Severity::Fatal,
+            issue_type: issues::IssueType::UnloadableModel,
+            object_id: "A fatal error has occured while loading the model, many rules have not been checked".to_string(),
+            object_type: None,
+            object_name: None,
+            related_objects: vec![],
+            details: Some(
+                "error while reading stops.txt: CSV deserialize error: record 12 (line: 13, byte: 739): invalid float literal".to_string()
+            )
+        });
+
+    // a nice feature is that even if the model was unloadable, we can check some rules
+    // there we can check that a trip id is missing (but we don't check the stops's id, as they are all missing, since we can't read the stops.txt file)
+    assert_eq!(
+        issues.validations[&issues::IssueType::InvalidReference],
+        vec![issues::Issue {
+            severity: issues::Severity::Fatal,
+            issue_type: issues::IssueType::InvalidReference,
+            object_id: "AAMV".to_string(),
+            object_type: Some(gtfs_structures::ObjectType::Route),
+            object_name: None,
+            related_objects: vec![issues::RelatedObject {
+                id: "AAMV4".to_string(),
+                object_type: Some(gtfs_structures::ObjectType::Trip),
+                name: Some("route id: AAMV, service id: WE".to_string())
+            }],
+            details: Some("The route is referenced by a trip but does not exists".to_string())
+        }]
+    );
 }
