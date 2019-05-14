206f5301f86dfed4e3b831479c4038b99a468757
Tristram Gräbener <tristramg@gmail.com>
2019-05-14 17:35:02 +0200

    Allow to post directly a file to validate

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,5 +1,6 @@
-use crate::validators::{create_issues, Response};
-use actix_web::{get, web::Json, web::Query, App, HttpServer, Responder};
+use crate::validators::{create_issues, create_issues_from_reader, Response};
+use actix_web::{get, post, web, web::Json, App, Error, HttpServer, Responder};
+use futures::{Future, Stream};
 use std::env;
 
 #[derive(Deserialize)]
@@ -8,8 +9,13 @@ struct Params {
     max_size: Option<usize>,
 }
 
+#[derive(Deserialize)]
+struct PostParams {
+    max_size: Option<usize>,
+}
+
 #[get("/validate")]
-fn validate(params: Query<Params>) -> Json<Response> {
+fn validate(params: web::Query<Params>) -> Json<Response> {
     let i = create_issues(&params.url, params.max_size.unwrap_or(1000));
     log::info!("Finished validation: {}", &params.url);
     Json(i)
@@ -21,14 +27,36 @@ fn index() -> impl Responder {
 Use it with /validate?url=https.//.../gtfs.zip"#
 }
 
+#[post("/validate")]
+fn validate_post(
+    params: web::Query<PostParams>,
+    body: web::Payload,
+) -> impl Future<Item = Json<Response>, Error = Error> {
+    let max_size = params.max_size.unwrap_or(1000);
+    body.map_err(Error::from)
+        .fold(web::BytesMut::new(), move |mut body, chunk| {
+            body.extend_from_slice(&chunk);
+            Ok::<_, Error>(body)
+        })
+        .and_then(move |body| {
+            let reader = std::io::Cursor::new(body);
+            Ok(Json(create_issues_from_reader(reader, max_size)))
+        })
+}
+
 pub fn run_server() {
     let port = env::var("PORT").unwrap_or_else(|_| "7878".to_string());
     let bind = env::var("BIND").unwrap_or_else(|_| "127.0.0.1".to_string());
     let addr = format!("{}:{}", bind, port);
 
-    HttpServer::new(|| App::new().service(validate).service(index))
-        .bind(addr.clone())
-        .expect(&format!("impossible to bind address {}", &addr))
-        .run()
-        .unwrap()
+    HttpServer::new(|| {
+        App::new()
+            .service(validate)
+            .service(index)
+            .service(validate_post)
+    })
+    .bind(addr.clone())
+    .expect(&format!("impossible to bind address {}", &addr))
+    .run()
+    .unwrap()
 }
