c6aff7acfe9f67b1c26d60259ef6590d3e3588bd
Francis <fchabouis@gmail.com>
2021-08-02 12:21:11 +0200

    fix code style

--- src/visualization.rs
+++ src/visualization.rs
@@ -8,22 +8,18 @@ pub fn add_issue_visualization(issue: &mut issues::Issue, gtfs: &Gtfs) {
     match issue.object_type {
         Some(ObjectType::Stop) => {
             let stop_id = issue.object_id.clone();
-            let stop_feature = geojson_feature_point(&stop_id, gtfs);
-            let mut related_stops_features: Vec<Option<Feature>> = get_related_stops(issue)
+
+            // a vec containing the stop_id and the related stop ids features
+            let features = [stop_id]
                 .iter()
+                .chain(get_related_stop_ids(issue).iter())
                 .map(|stop_id| geojson_feature_point(&stop_id, gtfs))
+                .flatten()
                 .collect();
-
-            // il doit exister plus simple...
-            let mut features = vec![];
-            features.push(stop_feature);
-            features.append(&mut related_stops_features);
-
-            let filtered_features = features.into_iter().flatten().collect();
 
             let feature_collection = FeatureCollection {
                 bbox: None,
-                features: filtered_features,
+                features: features,
                 foreign_members: None,
             };
 
@@ -34,21 +30,16 @@ pub fn add_issue_visualization(issue: &mut issues::Issue, gtfs: &Gtfs) {
 }
 
 fn geojson_feature_point(stop_id: &String, gtfs: &Gtfs) -> Option<Feature> {
-    let obj = gtfs.stops.get(stop_id);
-    return match obj {
-        Some(stop) => {
-            let stop_geom = get_stop_geom(stop);
-            let feature = Feature {
-                geometry: stop_geom,
-                bbox: None,
-                properties: None,
-                id: None,
-                foreign_members: None,
-            };
-            Some(feature)
+    gtfs.stops.get(stop_id).map(|stop| {
+        let stop_geom = get_stop_geom(stop);
+        Feature {
+            geometry: stop_geom,
+            bbox: None,
+            properties: None,
+            id: None,
+            foreign_members: None,
         }
-        None => None,
-    };
+    })
 }
 
 fn get_stop_geom(stop: &Arc<gtfs_structures::Stop>) -> Option<geojson::Geometry> {
@@ -58,11 +49,11 @@ fn get_stop_geom(stop: &Arc<gtfs_structures::Stop>) -> Option<geojson::Geometry>
     }
 }
 
-fn get_related_stops(issue: &issues::Issue) -> Vec<String> {
+fn get_related_stop_ids(issue: &issues::Issue) -> Vec<String> {
     let related_objects = &issue.related_objects;
-    return related_objects
+    related_objects
         .iter()
         .filter(|o| o.object_type == Some(ObjectType::Stop))
         .map(|s| s.id.clone())
-        .collect();
+        .collect()
 }
