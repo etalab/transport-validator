d1c8fb6a25c4d7df04cc064e4b13afcbf259f294
Francis Chabouis <fchabouis@gmail.com>
2022-11-15 14:39:38 +0100

    add a check for unused shapes

--- src/validators/shapes.rs
+++ src/validators/shapes.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::issues::*;
 
 pub fn validate(gtfs: &gtfs_structures::Gtfs) -> Vec<Issue> {
@@ -20,13 +22,38 @@ pub fn validate(gtfs: &gtfs_structures::Gtfs) -> Vec<Issue> {
     let valid_shape_id = gtfs
         .trips
         .iter()
-        .map(|(_id, trip)| invalid_shape_id(trip, gtfs))
+        .map(|(_id, trip)| create_invalid_shape_id_issue(trip, gtfs))
         .filter_map(|o| o);
 
-    missing_coord.chain(valid).chain(valid_shape_id).collect()
+    let used_shape_id: HashSet<String> = gtfs
+        .trips
+        .iter()
+        .map(|(_trip_id, trip)| trip.shape_id.to_owned())
+        .filter_map(|o| o)
+        .collect();
+
+    let existing_shape_id: HashSet<String> = gtfs
+        .shapes
+        .iter()
+        .map(|(shape_id, _shape)| shape_id.to_owned())
+        .collect();
+
+    let unused_shape_id = existing_shape_id.difference(&used_shape_id).map(|id| {
+        Issue::new(Severity::Warning, IssueType::UnusedShapeId, id)
+            .object_type(gtfs_structures::ObjectType::Shape)
+    });
+
+    missing_coord
+        .chain(valid)
+        .chain(valid_shape_id)
+        .chain(unused_shape_id)
+        .collect()
 }
 
-fn invalid_shape_id(trip: &gtfs_structures::Trip, gtfs: &gtfs_structures::Gtfs) -> Option<Issue> {
+fn create_invalid_shape_id_issue(
+    trip: &gtfs_structures::Trip,
+    gtfs: &gtfs_structures::Gtfs,
+) -> Option<Issue> {
     match &trip.shape_id {
         None => None,
         Some(shape_id) => {
@@ -83,4 +110,32 @@ fn test_valid() {
         IssueType::InvalidCoordinates,
         invalid_coord_issue[0].issue_type
     );
+}
+
+#[test]
+fn test_invalid_shape_id() {
+    let gtfs = gtfs_structures::Gtfs::new("test_data/shapes").unwrap();
+    let issues = validate(&gtfs);
+    let invalid_shape_id: Vec<_> = issues
+        .iter()
+        .filter(|issue| issue.issue_type == IssueType::InvalidShapeId)
+        .collect();
+
+    assert_eq!(1, invalid_shape_id.len());
+    assert_eq!("AB1", invalid_shape_id[0].object_id);
+    assert_eq!(IssueType::InvalidShapeId, invalid_shape_id[0].issue_type);
+}
+
+#[test]
+fn test_unused_shape_id() {
+    let gtfs = gtfs_structures::Gtfs::new("test_data/shapes").unwrap();
+    let issues = validate(&gtfs);
+    let unused_shape_id: Vec<_> = issues
+        .iter()
+        .filter(|issue| issue.issue_type == IssueType::UnusedShapeId)
+        .collect();
+
+    assert_eq!(1, unused_shape_id.len());
+    assert_eq!("A_shp", unused_shape_id[0].object_id);
+    assert_eq!(IssueType::UnusedShapeId, unused_shape_id[0].issue_type);
 }
