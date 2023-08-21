d545b7fea174984b51b6c931d2cfd539d5744b95
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2023-08-21 22:40:31 +0200

    Add GitHub and documentation link on web homepage (#167)
    * Add GitHub and documentation link on web homepage
    
    * Apply suggestion
    
    Co-authored-by: Tristram Gräbener <tristramg@gmail.com>
    
    ---------
    
    Co-authored-by: Tristram Gräbener <tristramg@gmail.com>

--- src/daemon.rs
+++ src/daemon.rs
@@ -32,7 +32,10 @@ async fn validate(params: web::Query<Params>) -> Result<Json<Response>, Error> {
 #[get("/")]
 async fn index() -> &'static str {
     r#"GTFS Validation tool (https://github.com/etalab/transport-validator-rust)
-Use it with /validate?url=https://.../gtfs.zip"#
+Use it with /validate?url=https://.../gtfs.zip
+
+This software is open-source and distributed under the MIT license.
+See the code and the documentation: https://github.com/etalab/transport-validator"#
 }
 
 #[post("/validate")]
